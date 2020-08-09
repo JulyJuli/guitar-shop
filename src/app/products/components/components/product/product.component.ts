0b819d9748cdb97665257e26e13ac45c4fea74b3
Yuliia Nechyporuk <Yuliia_Nechyporuk@epam.com>
2020-08-09 11:23:09 +0300

    extend changelog file

--- src/app/products/components/components/product/product.component.ts
+++ src/app/products/components/components/product/product.component.ts
@@ -1,15 +1,23 @@
-import { Component, Input, Output, EventEmitter, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnDestroy, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 
 import { ProductModel } from 'src/app/products/models/product.model';
 
 @Component({
   selector: 'app-product-component',
   templateUrl: './product.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styles: ['./product.component.css']
 })
 export class ProductComponent implements OnDestroy {
   @Input() sourceProduct: ProductModel;
   @Output() boughtProduct = new EventEmitter<ProductModel>();
+
+  constructor(private ref: ChangeDetectorRef) {
+    ref.detach();
+    setInterval(() => {
+      this.ref.detectChanges();
+    }, 500);
+ }
 
   ngOnDestroy(): void {
     this.boughtProduct.unsubscribe();
