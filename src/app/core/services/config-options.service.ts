5ab5ed3f09ac48c4b9f7f070c3bd69e0f3468565
Yuliia Nechyporuk <Yuliia_Nechyporuk@epam.com>
2020-08-11 16:41:40 +0300

    task 3

---
+++ src/app/core/services/config-options.service.ts
@@ -0,0 +1,24 @@
+export class ConfigOptionsService {
+    public config = {
+        id: 1,
+        login: 'admin',
+        email: 'admin@mail.com'
+      };
+    
+      constructor() { }
+    
+      getConfig(field: string): object {
+        if (this.isConfigExist(field)) {
+          return { field: this.config[field] };
+        }
+      }
+    
+      setConfig(newData: {}): object {
+        return Object.assign(this.config, newData);
+      }
+    
+      isConfigExist(field: string): boolean {
+        return Object.keys(this.config).some(key => key === field);
+      }
+ };
+ 
