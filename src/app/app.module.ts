f5dd5ca207d29a42fb532a2405eae430967bae4d
Yuliia Nechyporuk <Yuliia_Nechyporuk@epam.com>
2020-09-14 21:43:02 +0300

    add timing interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/components/first/first.component';
@@ -10,6 +10,8 @@ import { ProductModule } from './products/product.module';
 import { SharedModule } from './shared/shared.module';
 import { AboutComponent } from './layout/components/about.component';
 import { OrderModule } from './orders/order.module';
+import { httpInterceptorProviders } from './core/interceptors';
+import { TimingInterceptor } from './core/interceptors/timing.interceptor';
 
 @NgModule({
   declarations: [
@@ -25,6 +27,13 @@ import { OrderModule } from './orders/order.module';
     OrderModule,
     HttpClientModule,
     AppRoutingModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TimingInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })
